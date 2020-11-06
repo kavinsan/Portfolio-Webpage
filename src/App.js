c92a6527fa912ee7f3dd9c471ec2fd72cc6c7b8d
kavinsan <kavin3103@hotmail.com>
2020-11-05 23:56:07 -0500

    Components

--- src/App.js
+++ src/App.js
@@ -11,63 +11,126 @@ const App = (props) => {
       <div className="LeftPanel">
         <ContentTitle>Profile</ContentTitle>
         <Content>
-          Bacon ipsum dolor amet beef ribs doner strip steak kielbasa tenderloin
-          kevin ham hock turducken pancetta prosciutto. Shoulder buffalo fatback
-          landjaeger cow chicken pig capicola. Pastrami pork loin pork belly
-          kevin prosciutto swine. Pastrami ground round venison, t-bone turkey
-          tongue short ribs salami. Rump shankle tri-tip bresaola turkey short
-          ribs alcatra ground round. Capicola drumstick flank tongue. Doner
-          drumstick beef chislic biltong chuck, meatloaf meatball sausage
-          tongue.
+          A full stack Software Engineer and a life-long learner focused on new
+          technology and best practices to write beautiful code. Looking for a
+          full-time position at a company to further understandings in backend
+          architecture in large scale project design as well as fundamentals of
+          frontend UX/UI designs. Experienced in object oriented programming,
+          software development processes and working in an agile startup
+          environments.
         </Content>
         <PortfolioBlock />
         <ContentTitle>Work Experience</ContentTitle>
         <Content>
-          Bacon ipsum dolor amet beef ribs doner strip steak kielbasa tenderloin
-          kevin ham hock turducken pancetta prosciutto. Shoulder buffalo fatback
-          landjaeger cow chicken pig capicola. Pastrami pork loin pork belly
-          kevin prosciutto swine. Pastrami ground round venison, t-bone turkey
-          tongue short ribs salami. Rump shankle tri-tip bresaola turkey short
-          ribs alcatra ground round. Capicola drumstick flank tongue. Doner
-          drumstick beef chislic biltong chuck, meatloaf meatball sausage
-          tongue.
+          <span>April 2019 - Present</span>
+          <span>Surge ERP Consulting | Markham, ON</span>
+          <span>Full Stack Software Developer</span>
+          <ul>
+            <li>
+              Developed a website including a landing page, login and user
+              functionality.{" "}
+            </li>
+            <li>Developed the UI for an image editing tool in React JS. </li>
+            <li>
+              Implemented service controllers on a Express server using RESTful
+              API methodologies.{" "}
+            </li>
+            <li>Dynamic image generation using Python 3</li>
+            <li>Dynamic service side SVG generation in Node, Express</li>
+          </ul>
         </Content>
         <ContentTitle>Project</ContentTitle>
         <Content>
-          Bacon ipsum dolor amet beef ribs doner strip steak kielbasa tenderloin
-          kevin ham hock turducken pancetta prosciutto. Shoulder buffalo fatback
-          landjaeger cow chicken pig capicola. Pastrami pork loin pork belly
-          kevin prosciutto swine. Pastrami ground round venison, t-bone turkey
-          tongue short ribs salami. Rump shankle tri-tip bresaola turkey short
-          ribs alcatra ground round. Capicola drumstick flank tongue. Doner
-          drumstick beef chislic biltong chuck, meatloaf meatball sausage
-          tongue.
+          <div>CryptoMibs Platform - React, Web3, Express</div>
+          <ul>
+            <li>
+              A decentralized application using Web3 to communicate with the
+              Ethereum blockchain. Front-end built in React/Redux.
+            </li>
+            <li>
+              Developed an image editing web app, Express server to handle
+              service controls, Python 3 for image manipulation and Front-end
+              built in React/Redux.
+            </li>
+          </ul>
+          <div>Google Maps - Node, Express, Python 3</div>
+          <ul>
+            <li>
+              A bot developed to manage and interact with users on a live
+              discord server. Using the Discord API to manage user events, role
+              based commands and server side service controllers
+            </li>
+            <li>
+              Utilizes web scrapers to fetch user stats for certain online games
+              that don’t provide APIs.
+            </li>
+          </ul>
+          <div>Discord Bot - Node, Express, Python 3</div>
+          <ul>
+            <li>
+              A bot developed to manage and interact with users on a live
+              discord server. Using the Discord API to manage user events, role
+              based commands and server side service controllers
+            </li>
+            <li>
+              Utilizes web scrapers to fetch user stats for certain online games
+              that don’t provide APIs.
+            </li>
+          </ul>
         </Content>
       </div>
       <div className="RightPanel">
-        <ContentTitle>Contact</ContentTitle>
         <Content>
-          Bacon ipsum dolor amet beef ribs doner strip steak kielbasa tenderloin
-          kevin ham hock turducken pancetta prosciutto. Shoulder buffalo fatback
-          landjaeger cow chicken pig capicola. Pastrami pork loin pork belly
-          kevin prosciutto swine. Pastrami ground round venison, t-bone turkey
-          tongue short ribs salami. Rump shankle
+          <ContentTitle>Contact</ContentTitle>
+          <div>
+            <ul>
+              <li>647-896-4103</li>
+              <li>kavinsan.nesan@gmail.com</li>
+              <li>github.com/kavinsan</li>
+              <li>linkedin.com/in/kavinsan</li>
+            </ul>
+          </div>
         </Content>
-        <ContentTitle>Skills</ContentTitle>
         <Content>
-          Bacon ipsum dolor amet beef ribs doner strip steak kielbasa tenderloin
-          kevin ham hock turducken pancetta prosciutto. Shoulder buffalo fatback
-          landjaeger cow chicken pig capicola. Pastrami pork loin pork belly
-          kevin prosciutto swine. Pastrami ground round venison, t-bone turkey
-          tongue short ribs salami. Rump shankle tri-tip bresaola turkey short
-          ribs alcatra ground round. Capicola drumstick flank tongue. Doner
-          drumstick beef chislic biltong chuck, meatloaf meatball sausage
-          tongue.
+          <ContentTitle>Skills</ContentTitle>
+          <h2>Languages</h2>
+          <ul>
+            <li>JavaScript - 2 Years</li>
+            <li>Python 3 - 2 Years</li>
+            <li>Java</li>
+            <li>C - 1 Year</li>
+          </ul>
+          <h2>Front-End</h2>
+          <ul>
+            <li>HTML 5</li>
+            <li>CSS 3</li>
+            <li>React JS &amp; Redux Saga</li>
+            <li>Vue JS</li>
+            <li>Bootsrap, Material UI</li>
+          </ul>
+          <h2>Back-End</h2>
+          <ul>
+            <li>Node JS</li>
+            <li>Express JS</li>
+            <li>AWS</li>
+            <ul>
+              <li>CloudFront</li>
+              <li>S3</li>
+              <li>Lambda</li>
+            </ul>
+          </ul>
+          <h2>Database</h2>
+          <ul>
+            <li>SQL</li>
+            <li>MongoDB</li>
+            <li>GraphQL</li>
+          </ul>
         </Content>
-        <ContentTitle>Education</ContentTitle>
         <Content>
-          Bacon ipsum dolor amet beef ribs doner strip steak kielbasa tenderloin
-          kevin ham hock turducken pancetta prosciutto. Shoulder
+          <ContentTitle>Education</ContentTitle>
+          <span>2014 - 2018</span>
+          <span>BACHELORS OF COMPUTER SCIENCE</span>
+          <span>Ryerson University</span>
         </Content>
       </div>
     </div>
