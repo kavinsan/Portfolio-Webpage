bc4f5480f7f51aac9eac94576aaaf66f295c3c76
kavinsan <kavin3103@hotmail.com>
2020-11-07 18:25:13 -0500

    Responsive

--- src/components/contactsPanel/styles.js
+++ src/components/contactsPanel/styles.js
@@ -39,11 +39,9 @@ export const useStyles = makeStyles((theme) => ({
     },
     [theme.breakpoints.down("870")]: {
       width: "240px",
-    },
-    [theme.breakpoints.down("xs")]: {
       "& .contact-value": {
         color: "white",
       }
-    },
+    }
   },
 }));
