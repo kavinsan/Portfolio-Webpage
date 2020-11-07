64d5821409c40dc90f632cb5ca21d6121bf0db3b
kavinsan <kavin3103@hotmail.com>
2020-11-07 16:13:34 -0500

    Updates

---
+++ precache-manifest.ea87024468adc0e056666f81c19cde71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e788efa8fffc1b8ecff95dc0934f7948",
+    "url": "/Portfolio-Webpage/index.html"
+  },
+  {
+    "revision": "f9cda89569c674896c70",
+    "url": "/Portfolio-Webpage/static/css/main.59061972.chunk.css"
+  },
+  {
+    "revision": "c0cdf89762960384c54e",
+    "url": "/Portfolio-Webpage/static/js/2.7c0087da.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio-Webpage/static/js/2.7c0087da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9cda89569c674896c70",
+    "url": "/Portfolio-Webpage/static/js/main.e094ccdf.chunk.js"
+  },
+  {
+    "revision": "d5663cfca6e770a1cae8",
+    "url": "/Portfolio-Webpage/static/js/runtime-main.b8c56bb3.js"
+  },
+  {
+    "revision": "5c424d02518a90a363873009ed7286aa",
+    "url": "/Portfolio-Webpage/static/media/profile-image.5c424d02.png"
+  }
+]);
